6fa5d9be78e0e7e56c36072fd55a88915c5fc541
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2022-05-04 12:40:42 -0500

    2 Pantallas

--- app/src/main/java/com/example/reto2/MainActivity.kt
+++ app/src/main/java/com/example/reto2/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.reto2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.reto2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //binding
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view= binding.root
+        setContentView(view)
     }
 }
