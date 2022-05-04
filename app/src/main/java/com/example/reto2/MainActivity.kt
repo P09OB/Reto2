e048d425587d6b0feaa0e7d7e8bb3fdf1b075027
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2022-05-04 17:29:03 -0500

    Firebase listo

--- app/src/main/java/com/example/reto2/MainActivity.kt
+++ app/src/main/java/com/example/reto2/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.reto2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.reto2.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import model.User
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +24,47 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         val view= binding.root
         setContentView(view)
+
+        /*binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, Pokedex::class.java))
+        }*/
+
+
+        binding.loginBtn.setOnClickListener {
+            val username = binding.loginNombreUser.text.toString()
+
+            Firebase.firestore.collection("users")
+                .document(username).get().addOnSuccessListener {
+                    //El usuario ya esta
+                    val user = it.toObject(User::class.java)
+                    Log.e("Ya esta", user.toString())
+
+                    startActivity(Intent(this, Pokedex::class.java))
+
+                    //El usuario no esta, pero lo creamos
+                    if (user == null) {
+                        registerUserData()
+                    }
+                }
+        }
+    }
+
+
+    fun registerUserData() {
+        //uid
+        val uid= Firebase.auth.currentUser?.uid
+        uid?.let{}
+
+        val user= User(
+            UUID.randomUUID().toString(),
+            binding.loginNombreUser.text.toString()
+        )
+
+        Firebase.firestore.collection("users").document(binding.loginNombreUser.text.toString()).set(user).addOnSuccessListener {
+            Log.e("No estaba", "apenas creamos" +user)
+        }
+
+        startActivity(Intent(this, Pokedex::class.java))
+
     }
 }
